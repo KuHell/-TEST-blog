24f4efaf9b597ae7c65de8d5ac1771ec557f1b11
KuHell <itkdgus489@gmail.com>
2022-08-29 16:33:38 +0900

    write form 제작

--- pages/post/write.js
+++ pages/post/write.js
@@ -1,5 +1,34 @@
 import Layout from '../components/Layout'
+import { useRef } from 'react'
 
 export default function Write() {
-  return <Layout>write</Layout>
+  const idRef = useRef()
+  const titleRef = useRef()
+  const contentRef = useRef()
+
+  const handleSubmit = () => {}
+  return (
+    <Layout>
+      <h1>write a post</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="id" placeholder="id" required ref={idRef} />
+        <br />
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          required
+          ref={titleRef}
+        />
+        <br />
+        <textarea
+          type="text"
+          name="content"
+          placeholder="content"
+          required
+          ref={contentRef}
+        />
+      </form>
+    </Layout>
+  )
 }
