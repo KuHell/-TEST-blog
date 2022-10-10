5b6626c02a4f55cd9d672ff20d5e1902a0549304
KuHell <itkdgus489@gmail.com>
2022-10-10 22:21:28 +0900

    test

--- pages/api/post/write.js
+++ pages/api/post/write.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
     console.log('title: ', title)
     console.log('title: ', title)
     console.log('title: ', title)
+    console.log('title: ', title)
     // console.log('date: ', date)
     console.log('content: ', content)
   }
